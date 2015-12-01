104f2fba7906aa4290c7a322a53778790cc27090
Jaelyn <1004177537@qq.com>
2015-12-02 07:09:55 +0800

    download1

---
+++ Download_Demo/src/main/java/com/Download/db/DBHaper.java
@@ -0,0 +1,36 @@
+package com.Download.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Jaelyn on 2015/12/1.
+ * 数据库帮助类
+ */
+public class DBHaper extends SQLiteOpenHelper{
+
+    private static final String DB_NAME = "download.db";//数据库名
+    private static final int VERSION = 1;  //数据库版本
+    //创建表
+    private static final String SQL_CREATE = "create table thread_info(_id integer primary key autoincrement,thread_id integer,url text,start integer,end integer,finished integer)";
+    //删除表
+    private static final String SQL_DROP = "drop table if exists thread_info";
+
+    public DBHaper(Context context) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+         db.execSQL(SQL_CREATE);//创建
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //更新数据库
+        db.execSQL(SQL_DROP);//删除数据库
+        db.execSQL(SQL_CREATE);//创建
+    }
+}

83fd68a677b84f8eb1c4dcf0ff392a7b412606ed
Jaelyn <1004177537@qq.com>
2015-12-02 07:10:24 +0800

    download2

---
+++ Download_Demo/src/main/java/com/Download/db/ThreadDAOImpl.java
@@ -0,0 +1,81 @@
+package com.Download.db;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.jaelyn.entities.ThreadInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 数据访问接口实现
+ * Created by Jaelyn on 2015/12/2.
+ */
+public class ThreadDAOImpl implements ThreadDAO{
+
+    private DBHaper mHaper = null;
+
+    public ThreadDAOImpl(Context context) {
+        mHaper = new DBHaper(context);
+    }
+
+
+    @Override
+    public void insertThread(ThreadInfo threadInfo) {
+        SQLiteDatabase db = mHaper.getWritableDatabase();//获取一个可以写的数据库
+        db.execSQL("insert into thread_info(thread_id,url,start,end,finished) values(?,?,?,?,?)",
+                new Object[]{threadInfo.getId(),
+                             threadInfo.getUrl(),
+                             threadInfo.getStart(),
+                             threadInfo.getEnd(),
+                             threadInfo.getFinished()});
+        db.close();
+    }
+
+    @Override
+    public void deleteThread(String url, int thread_id) {
+        SQLiteDatabase db = mHaper.getWritableDatabase();//获取一个可以写的数据库
+        db.execSQL("delete from thread_info where url = ? and thread_id = ?",
+                new Object[]{url,thread_id});
+        db.close();
+    }
+
+    @Override
+    public void updateThread(String url, int thread_id, int finished) {
+        SQLiteDatabase db = mHaper.getWritableDatabase();//获取一个可以写的数据库
+        db.execSQL("update thread_info set finished = ? where url = ? and thread_id = ?",
+                new Object[]{finished,url,thread_id});
+        db.close();
+    }
+
+    @Override
+    public List<ThreadInfo> getThread(String url) {
+        SQLiteDatabase db = mHaper.getWritableDatabase();//获取一个可以写的数据库
+        List<ThreadInfo> list = new ArrayList<ThreadInfo>();
+        Cursor cursor = db.rawQuery("select * from thread_info where url = ?",new String[]{url});
+        while (cursor.moveToNext()){
+            ThreadInfo threadInfo = new ThreadInfo();
+            threadInfo.setId(cursor.getInt(cursor.getColumnIndex("thread_id")));
+            threadInfo.setUrl(cursor.getString(cursor.getColumnIndex("url")));
+            threadInfo.setStart(cursor.getInt(cursor.getColumnIndex("start")));
+            threadInfo.setEnd(cursor.getInt(cursor.getColumnIndex("end")));
+            threadInfo.setFinished(cursor.getInt(cursor.getColumnIndex("finished")));
+            list.add(threadInfo);
+        }
+        cursor.close();
+        db.close();
+        return list;
+    }
+
+    @Override
+    public Boolean isExists(String url, int thread_id) {
+        SQLiteDatabase db = mHaper.getWritableDatabase();//获取一个可以写的数据库
+        Cursor cursor = db.rawQuery("select * from thread_info where url = ? and thread_id = ?", new String[]{url, thread_id + ""});
+        boolean exists = cursor.moveToNext();
+        cursor.close();
+        db.close();
+        return exists;
+    }
+}
